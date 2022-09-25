21c6cdd914b7517f8f8dd398a42983297b5ad0fe
Marcin Bigos <marcin.bigos99@gmail.com>
2022-09-25 22:00:52 +0200

    interface controller

--- src/Economius/Economius.BotRunner/Areas/Configuration/Controllers/ConfigurationController.cs
+++ src/Economius/Economius.BotRunner/Areas/Configuration/Controllers/ConfigurationController.cs
@@ -3,7 +3,12 @@ using Economius.BotRunner.Areas.Configuration.Commands;
 
 namespace Economius.BotRunner.Areas.Configuration.Controllers
 {
-    public class ConfigurationController
+    public interface IConfigurationController
+    {
+        Task SetupServer(SocketSlashCommand rawCommand, SetupServerCommand command);
+    }
+
+    public class ConfigurationController : IConfigurationController
     {
         public Task SetupServer(SocketSlashCommand rawCommand, SetupServerCommand command)
         {

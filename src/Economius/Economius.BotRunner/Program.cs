8611653571c74a4f0f2795634748fcc36f8acaf6
Marcin Bigos <marcin.bigos99@gmail.com>
2022-09-25 18:44:43 +0200

    suppress warning - nullable client

--- src/Economius/Economius.BotRunner/Program.cs
+++ src/Economius/Economius.BotRunner/Program.cs
@@ -14,7 +14,9 @@ namespace Economius.BotRunner
 {
     public class Program
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         private DiscordSocketClient _client;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         public static Task Main(string[] args) => new Program().MainAsync(args);
 

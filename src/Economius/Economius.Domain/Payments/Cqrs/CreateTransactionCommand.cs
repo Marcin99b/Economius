f4c0532d97c5d1227579152e4e87f7e2d866ca50
Marcin Bigos <marcin.bigos99@gmail.com>
2022-09-28 18:55:30 +0200

    CreateRevertTransactionByIdCommand

--- src/Economius/Economius.Domain/Payments/Cqrs/CreateTransactionCommand.cs
+++ src/Economius/Economius.Domain/Payments/Cqrs/CreateTransactionCommand.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Economius.Domain.Payments.Cqrs
 {
@@ -38,6 +39,38 @@ namespace Economius.Domain.Payments.Cqrs
             using var session = this.sessionFactory.CreateMongo();
             var transaction = new Transaction(command.ServerId, command.FromUserId, command.ToUserId, command.Amount);
             return session.AddAsync(transaction);
+        }
+    }
+
+    public class CreateRevertTransactionByIdCommand : ICommand
+    {
+        public Guid TransactionId { get; }
+
+        public CreateRevertTransactionByIdCommand(Guid transactionId)
+        {
+            this.TransactionId = transactionId;
+        }
+    }
+
+    public class CreateRevertTransactionByIdCommandHandler : ICommandHandler<CreateRevertTransactionByIdCommand>
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        public CreateRevertTransactionByIdCommandHandler(ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public Task HandleAsync(CreateRevertTransactionByIdCommand command)
+        {
+            using var session = this.sessionFactory.CreateMongo();
+            var found = session.Get<Transaction>(command.TransactionId);
+            if(found == null)
+            {
+                throw new ArgumentException($"Transaction with id {command.TransactionId} does not exist");
+            }
+            var revertedTransaction = new Transaction(found.ServerId, found.ToUserId, found.FromUserId, found.Amount);
+            return session.AddAsync(revertedTransaction);
         }
     }
 

5cf8740e9cce1d295121f3bac8a57f051e677e1d
Marcin Bigos <marcin.bigos99@gmail.com>
2022-09-29 19:55:46 +0200

    crud for product in shop

--- src/Economius/Economius.Domain/Shopping/Product.cs
+++ src/Economius/Economius.Domain/Shopping/Product.cs
@@ -2,7 +2,7 @@
 
 namespace Economius.Domain.Shopping
 {
-    public class Product : Entity
+    public class Product
     {
         public string Name { get; private set; }
         public long Price { get; private set; }
@@ -11,6 +11,30 @@ namespace Economius.Domain.Shopping
         {
             this.Name = name;
             this.Price = price;
+        }
+
+        internal void SetPrice(Action update, long price)
+        {
+            if(this.Price == price)
+            {
+                return;
+            }
+            this.Price = price;
+            update.Invoke();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj switch
+            {
+                Product p => p.Name == this.Name && p.Price == this.Price,
+                _ => false
+            };
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- src/Economius/Economius.Domain/Shopping/Shop.cs
+++ src/Economius/Economius.Domain/Shopping/Shop.cs
@@ -4,22 +4,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Economius.Domain.Shopping
 {
     public class Shop : Entity
     {
+        private List<Product> products = new List<Product>();
+
         public ulong ServerId { get; private set; }
         public ulong UserId { get; private set; }
         public ulong WalletId { get; private set; }
-        public IEnumerable<Product> Products { get; private set; }
+        public IEnumerable<Product> Products
+        {
+            get => this.products;
+            protected set => this.products = new List<Product>(value);
+        }
 
         public Shop(ulong serverId, ulong userId, ulong walletId)
         {
             this.ServerId = serverId;
             this.UserId = userId;
             this.WalletId = walletId;
-            this.Products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            if(this.products.Any(x => x.Name == product.Name))
+            {
+                throw new ArgumentException($"Product already exist in shop");
+            }
+            this.products.Add(product);
+            this.Update();
+        }
+
+        public void UpdateProduct(string name, long newPrice)
+        {
+            var product = this.products.FirstOrDefault(x => x.Name == name);
+            if (product == null)
+            {
+                throw new ArgumentException($"Product does not exist in shop");
+            }
+            product.SetPrice(this.Update, newPrice);
+        }
+
+        public void RemoveProduct(string name)
+        {
+            var product = this.products.FirstOrDefault(x => x.Name == name);
+            if (product == null)
+            {
+                throw new ArgumentException($"Product does not exist in shop");
+            }
+            this.products.Remove(product);
+            this.Update();
         }
     }
 }

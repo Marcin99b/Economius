5033b27212a4003f9b4ad97a720f2de7e4fcd094
Marcin Bigos <marcin.bigos99@gmail.com>
2022-09-30 19:40:11 +0200

    show errors in debug

--- src/Economius/Economius.BotRunner/CommandsRouter.cs
+++ src/Economius/Economius.BotRunner/CommandsRouter.cs
@@ -8,6 +8,7 @@ using Economius.BotRunner.Areas.Payments.Controllers;
 using Economius.BotRunner.Areas.Payments.Views;
 using MongoDB.Driver;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Economius.BotRunner
 {
@@ -22,13 +23,31 @@ namespace Economius.BotRunner
         {
             var arguments = rawCommand.Data.Options.Select(x => (Name: x.Name, Value: x.Value)).ToArray();
             var botCommand = WorkflowTypesContainer.AutoCreateCommand(rawCommand.Data.Name, arguments) as IBotCommand;
-
+#if DEBUG
+            var viewModel = await this.ShowErrorIfCatch(async () => await WorkflowTypesContainer.RunMethodOfCommand(rawCommand, botCommand, rawCommand.Data.Name));
+#else
+            //todo error processing
             var viewModel = await WorkflowTypesContainer.RunMethodOfCommand(rawCommand, botCommand, rawCommand.Data.Name);
-            if(viewModel == null)
+#endif
+
+            if (viewModel == null)
             {
                 throw new NullReferenceException();
             }
             await WorkflowTypesContainer.RunViewOfModel(rawCommand, viewModel, viewModel.GetType().Name);
+        }
+
+        private async Task<IViewModel> ShowErrorIfCatch(Func<Task<IViewModel>> func)
+        {
+            try
+            {
+                return await func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                var stackTraceWithoutFullPaths = Regex.Replace(ex.StackTrace ?? string.Empty, @"[A-Z]:((\\|\/)\w+)+(\\|\/)?Economius(\\|\/)src(\\|\/)", string.Empty);
+                return new ErrorViewModel($"**Raw errors are visible only in debug mode!**\n\n{ex.Message}\n\n{stackTraceWithoutFullPaths}");
+            }
         }
     }
 }
